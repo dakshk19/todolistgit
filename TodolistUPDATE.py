d30c94a795ffaf2feae1ddc5d6963ed7fb3529f8
dakshk19 <dakshk9999@gmail.com>
2025-06-13 00:57:12 +0530

    update

--- TodolistUPDATE.py
+++ TodolistUPDATE.py
@@ -31,37 +31,19 @@ def deltasks():
     conn.commit()
     print("task has been deleted")
 
-def markdone():
-    taskno = int(input("enter unique id of task which has to be mark done"))
-    x = input("press 0 if task is pending \n press 1 if task is done")
-    if x == "1":
-        mark = "update todo set status = 'Done' where uniqueID=%s"
-        mycursor.execute(mark,(taskno,))
-        conn.commit()
-        print("status marked as done")
-    elif x == "0":
-        mark = "update todo set status = 'pending' where uniqueID=%s"
-        mycursor.execute(mark,(taskno,))
-        conn.commit()
-        print("status marked as pending")
-    
 
 while True:
 
     print("press 1 to Add tasks\n") 
-    print("press 2 to Remove tasks\n")
-    print("press 3 to Mark task done/pending \n")
-    print("press 4 to See the TODOLIST\n")
+    print("press 2 to show tasks\n")
+    print("press 3 to remove tasks\n")
     x= input("choose any one option in the todo list\n")
 
     if x == "1":
         addtasks()
 
     elif x == "2":
-        deltasks()
+        showtask()
 
     elif x == "3":
-        markdone()
-
-    elif x == "4":
-        showlist()
+        deltasks()
